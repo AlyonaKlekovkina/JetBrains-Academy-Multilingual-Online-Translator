950b84aaedebccb427de7ef7a2df24f911109790
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-11-20 12:03:18 +0300

    Stage 3/7: Translations

--- translator.py
+++ translator.py
@@ -19,17 +19,44 @@ result = requests.get(url, headers=headers)
 
 if result:
     print('200 OK')
-    print('Translations')
+    print('\nContext examples:\n')
 
 soup = BeautifulSoup(result.content, 'html.parser')
-words = soup.find_all("a", {"class": "translation"})
+words = soup.find_all("a", {"class": "translation"}, {"lang": "fr"})
 list_of_words = []
 for i in words:
     list_of_words.append(i.text.strip())
-print(list_of_words)
 
-sentences = soup.find_all("div", {"class": "example"})
+
+sentences = soup.find_all("div", {"class": "example"}, {"lang": "fr"})
 list_of_sentences = []
 for i in sentences:
     list_of_sentences.append(i.text.strip())
-print(list_of_sentences)
+
+clean_sentences = []
+for i in range(len(list_of_sentences)):
+    pair = []
+
+    the_line = list_of_sentences[i].split('\n')
+    for j in range(len(the_line)):
+        pair.append(the_line[j].strip())
+    clean_sentences.append(pair)
+
+f = []
+for i in range(len(list_of_sentences)):
+    p = []
+    for j in clean_sentences[i]:
+        if j != ' ' and j != '' and j != '\n':
+            p.append(j.strip())
+    f.append(p)
+
+
+print("French Translations:")
+for i in range(1, 6):
+    print(list_of_words[i])
+
+print("\nFrench Examples:")
+for i in range(6):
+    for j in f[i]:
+        print(j)
+    print()
